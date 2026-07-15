5c6aed35d2e763001f389f222b4e1a4dfbbb5234
Samantha Paras <samantha.paras94@gmail.com>
2026-07-15 12:36:51 -0500

    Add spell details endpoint

--- src/routes/spells.js
+++ src/routes/spells.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getSpells } from "../controllers/spellsController.js";
+import { getSpells, getSpellById } from "../controllers/spellsController.js";
 
 const router = express.Router();
 
 router.get("/", getSpells);
+
+router.get("/:spellId", getSpellById);
 
 export default router;
